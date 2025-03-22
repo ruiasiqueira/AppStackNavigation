2c7e173d58cbac830d7ba8559a2335205e432921
ruiasiqueira <ruiamorim20046@gmail.com>
2025-03-21 21:00:39 -0300

    app

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -31,10 +31,7 @@ const styles = StyleSheet.create({
     container:{
         flex:1,
         justifyContent: 'center',
-         alignItems: 'center'
-
-
-    
+         alignItems: 'center'    
     }
 })
 
